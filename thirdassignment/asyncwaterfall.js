78f36dcc1facd24464813c7b0be48e39cab44b98
singlavipan18@gmail.com <singlavipan18@gmail.com>
2019-10-01 10:23:28 +0530

    added gitignore

--- thirdassignment/asyncwaterfall.js
+++ thirdassignment/asyncwaterfall.js
@@ -1,17 +1,47 @@
 var async = require('async');
 async.waterfall([
   function(callback) {
-    callback(null, 'Task 1', 'Task 2');
+    //user login 
+    var user = {
+      userName : "vipan",
+      emailId : "singlavipan18@gmail.com",
+      password : "abc123!",
+      phoneNo : "9803410390"
+    }
+    callback(null, user.userName, user.emailId,user.password,user.phoneNo);
+    console.log("hello 1");
   },
-  function(arg1, arg2, callback) {
-    // arg1 now equals 'Task 1' and arg2 now equals 'Task 2'
-    let arg3 = arg1 + ' and ' + arg2;
-    callback(null, arg3);
-  },
-  function(arg1, callback) {
-    // arg1 now equals 'Task1 and Task2'
-    arg1 += ' completed';
-    callback(null, arg1);
+  function(arg1, arg2,arg3, arg4, callback) {
+    // checking if user exists
+    //fetching user data from database
+    let userName = "vipan";
+    let emailId = "singlavipan18@gmail.com";
+    let password = "abc123!"
+    if(userName === arg1)
+    {
+      //user exists 
+    }
+    else
+    {
+      throw("user doesnot exists")
+    }
+    if(emailId===arg2)
+    {
+      // email id exists 
+    }
+    else 
+    {
+      throw("email id does not exists")
+    }
+    if(password===arg3)
+    {
+      // password matches
+    }
+    else 
+    {
+      throw("password doesnot matches")
+    }
+    callback(null, arg4);
   }
 ], function(err, result) {
   // result now equals to 'Task1 and Task2 completed'
@@ -19,3 +49,4 @@ async.waterfall([
 });
 
 // Or, with named functions:
+
